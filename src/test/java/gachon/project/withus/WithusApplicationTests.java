13967ffbafa8770e3af2608715d07425b486f185
SeungHwan <vw9801@naver.com>
2021-11-26 17:15:45 +0900

    withUs프로젝트 초기생성

---
+++ src/test/java/gachon/project/withus/WithusApplicationTests.java
@@ -0,0 +1,13 @@
+package gachon.project.withus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
