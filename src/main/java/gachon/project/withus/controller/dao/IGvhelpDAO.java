efd6ecbed1269f4d08dcf3a9649fdd4f8bf85218
choyoungwoo9 <choyoungwoo9@naver.com>
2021-12-03 07:49:29 +0900

    DTO, Controller 폼 작성

--- src/main/java/gachon/project/withus/controller/dao/IGvhelpDAO.java
+++ src/main/java/gachon/project/withus/controller/dao/IGvhelpDAO.java
@@ -1,7 +1,12 @@
 package gachon.project.withus.controller.dao;
 
+import gachon.project.withus.controller.dto.UserGvhelp.GetGvhelpRequestDTO;
+import gachon.project.withus.controller.dto.UserGvhelp.GetGvhelpResponseDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface IGvhelpDAO {
+    public List<GetGvhelpResponseDTO> gvhelp_list(GetGvhelpRequestDTO RequestDTO);
 }
